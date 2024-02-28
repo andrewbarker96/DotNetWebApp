bc040b4af3c1018ba679ac6c49eddf907b30486a
Andrew Barker <andrewbarker96@gmail.com>
2024-02-28 10:35:43 -0600

    Changes to be committed: 	new file:   bin/Debug/net8.0/DotNetWebApp 	new file:   bin/Debug/net8.0/DotNetWebApp.deps.json 	new file:   bin/Debug/net8.0/DotNetWebApp.dll 	new file:   bin/Debug/net8.0/DotNetWebApp.pdb 	new file:   bin/Debug/net8.0/DotNetWebApp.runtimeconfig.json 	new file:   bin/Debug/net8.0/appsettings.Development.json 	new file:   bin/Debug/net8.0/appsettings.json 	modified:   obj/Debug/net8.0/DotNetWebApp.AssemblyInfo.cs 	modified:   obj/Debug/net8.0/DotNetWebApp.AssemblyInfoInputs.cache 	new file:   obj/Debug/net8.0/DotNetWebApp.MvcApplicationPartsAssemblyInfo.cache 	new file:   obj/Debug/net8.0/DotNetWebApp.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net8.0/DotNetWebApp.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net8.0/DotNetWebApp.dll 	new file:   obj/Debug/net8.0/DotNetWebApp.genruntimeconfig.cache 	new file:   obj/Debug/net8.0/DotNetWebApp.pdb 	new file:   obj/Debug/net8.0/DotNetWebApp.sourcelink.json 	new file:   obj/Debug/net8.0/apphost 	new file:   obj/Debug/net8.0/ref/DotNetWebApp.dll 	new file:   obj/Debug/net8.0/refint/DotNetWebApp.dll 	new file:   obj/Debug/net8.0/staticwebassets.build.json 	new file:   obj/Debug/net8.0/staticwebassets/msbuild.build.DotNetWebApp.props 	new file:   obj/Debug/net8.0/staticwebassets/msbuild.buildMultiTargeting.DotNetWebApp.props 	new file:   obj/Debug/net8.0/staticwebassets/msbuild.buildTransitive.DotNetWebApp.props

--- obj/Debug/net8.0/DotNetWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e4c98cbdacd7794c977ec5c87438bdff481e35")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
