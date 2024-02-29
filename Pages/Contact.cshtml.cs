c41f186904c5f45f02d8c8924d6f362fda39e1d4
auspres <auspres33@gmail.com>
2024-02-29 16:16:33 -0600

    Verifies db is working

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -46,6 +46,11 @@ namespace DotNetWebApp.Pages
 
                 command.ExecuteNonQuery();
 
+                // Don't forget to close the connection... Better to define in a using tag
+                // so it doesn't have to be manually closed as shown in getAllContacts().
+                connection.Close();
+
+                getAllContacts();
                 //return new OkResult();
             }
             catch (Exception ex)
@@ -54,6 +59,39 @@ namespace DotNetWebApp.Pages
                 _logger.LogError($"Error: {ex.Message}");
                 //return new StatusCodeResult(500); // Internal Server Error
             }
+        }
+
+        private void getAllContacts()
+        {
+            string connectionString = "Data Source=|DataDirectory|/database.db;";
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Contact";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Access data using reader["ColumnName"]
+                            int id = Convert.ToInt32(reader["Id"]);
+                            string? name = reader["Name"].ToString();
+                            string? email = reader["Email"].ToString();
+                            string? message = reader["Message"].ToString();
+
+                            Console.WriteLine($"Id: {id}\n" +
+                                $"Name: {name}\n" +
+                                $"Email: {email}\n" +
+                                $"Message: {message}\n");
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
